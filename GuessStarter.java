fa0e30548af33662f5ac8444df4eb753ff9bb23c
cubicn101 <yuxinxia702@gmail.com>
2026-09-17 14:55:22 -0700

    Add files via upload

---
+++ GuessStarter.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+   public class GuessStarter {
+       public static void main(String[] args) {
+       //  pick a random number
+      
+       Random random = new Random();
+       int number = random.nextInt(100) + 1;
+       Scanner in = new Scanner(System.in);
+       
+       int guess = in.nextInt();
+       //IMPORTANT!!!!
+       int off = number - guess;
+       
+       System.out.println("I'm thinking of a number between 1 and 100");
+       System.out.println(" (Including both). Can you guess my number?");
+      
+     
+       System.out.println("Your guess is"+ guess + ".");
+       System.out.println("The number I was thinking of is:" + number);
+       System.out.println("You were off by:" + off);
+       
+  
+}
+}
