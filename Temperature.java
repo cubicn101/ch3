6d921bc0ae0bd046e29565bee4cf8132f06b99d7
cubicn101 <yuxinxia702@gmail.com>
2026-09-17 13:50:52 -0700

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Temperature {
+	// name must align to the package name?
+     
+     public static void main (String [] args) {
+		 
+     //double C_PER_F = (fahrenheit* 9.0/5) + 32;
+    
+     Scanner in = new Scanner(System.in);
+
+     System.out.println("Want to convert celsius to fahrenheit?");
+     int c = in.nextInt();
+     double f = c * 9.0/5.0 +32;
+     
+     System.out.printf("Equal to %.2f F", f );
+     // placeholder: that let the following words in the same quotation mark?
+   }
+}
